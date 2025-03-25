5f538051c55fc1ddbf606ae2b123906b983e4c13
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-03-25 09:08:12 +0300

    Добавил лейблы.

--- winForms ZZ/MainForm.Designer.cs
+++ winForms ZZ/MainForm.Designer.cs
@@ -32,11 +32,14 @@
             this.ListDisciplines = new System.Windows.Forms.ComboBox();
             this.EnteringNumber = new System.Windows.Forms.TextBox();
             this.Press = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // ListQueshions
             // 
-            this.ListQueshions.Location = new System.Drawing.Point(12, 12);
+            this.ListQueshions.Location = new System.Drawing.Point(12, 36);
             this.ListQueshions.Name = "ListQueshions";
             this.ListQueshions.Size = new System.Drawing.Size(443, 381);
             this.ListQueshions.TabIndex = 0;
@@ -46,7 +49,7 @@
             // 
             this.ListDisciplines.BackColor = System.Drawing.SystemColors.Info;
             this.ListDisciplines.FormattingEnabled = true;
-            this.ListDisciplines.Location = new System.Drawing.Point(480, 12);
+            this.ListDisciplines.Location = new System.Drawing.Point(480, 47);
             this.ListDisciplines.Name = "ListDisciplines";
             this.ListDisciplines.Size = new System.Drawing.Size(121, 21);
             this.ListDisciplines.TabIndex = 1;
@@ -67,11 +70,41 @@
             this.Press.Text = "Выбрать";
             this.Press.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Список вопросов";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Список дисциплин";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(486, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Выбор номеров вопросов";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Press);
             this.Controls.Add(this.EnteringNumber);
             this.Controls.Add(this.ListDisciplines);
@@ -89,6 +122,9 @@
         private System.Windows.Forms.ComboBox ListDisciplines;
         private System.Windows.Forms.TextBox EnteringNumber;
         private System.Windows.Forms.Button Press;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 

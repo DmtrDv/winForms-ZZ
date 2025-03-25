5b7799cd6f5d80e2e3b0a9bec285bd8dc9c92515
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-03-25 09:34:05 +0300

    Добавил в MyLib класс storage и код в него

---
+++ mylib/storage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mylib
+{
+    public class storage
+    {
+        List<string> Discipline = new List<string>();
+
+        List<string> ListQuiestions = new List<string>();
+
+        List<string> QuestionsForTest = new List<string>();
+        public void AddDiscipline(string List)
+        {
+            Discipline.Add(List);
+        }
+
+        public List<string> getDiscipline()
+        {
+            return Discipline;
+        }
+        public List<string> getListQuiestions()
+        {
+            return ListQuiestions;
+        }
+        public void AddQuestion(string List)
+        {
+            ListQuiestions.Add(List);
+        }
+        public void AddQuestionForTest(string List)
+        {
+            QuestionsForTest.Add(List);
+        }
+    }
+}
+

3a5a824fddd79ad97e61d6ff89750868f890937c
Andrey-Shelgunov <shelgunova3@gmail.com>
2025-03-25 09:25:49 +0300

    Начало переноса с консоли

--- winForms ZZ/MainForm.cs
+++ winForms ZZ/MainForm.cs
@@ -16,5 +16,33 @@ namespace winForms_ZZ
         {
             InitializeComponent();
         }
+        static void Main(string[] args)
+        {
+            const string Path = "FolderLists\\Disciple.txt";
+
+            storage AllDiscipline = new storage();
+            storage AllQuestions = new storage();
+
+            QuestionStreamReader ReaderDisciplines = new QuestionStreamReader();
+            QuestionStreamReader ReaderQuestions = new QuestionStreamReader();
+            ReaderDisciplines.ReadDisciplines(Path, AllDiscipline);
+
+            Console.WriteLine($"Введите одну из дисциплин: {String.Join(", ", AllDiscipline.getDiscipline())}");
+
+            string IntroducedDiscipline = Console.ReadLine();
+
+            Console.WriteLine("Вот все вопросы по дисциплине: \n");
+            Console.WriteLine(String.Join("\n", ReaderQuestions.ReadListQuestions(IntroducedDiscipline, AllQuestions).getListQuiestions()));
+
+            Console.WriteLine("Введите необходимые номера вопросов для тестов: ");
+            int numbersQuestions = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine(numbersQuestions);
+
+
+            Console.ReadKey();
+        }
     }
 }
+
